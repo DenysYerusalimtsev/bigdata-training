f37f5a32d056fa7b23e7e4559527796368614dca
Denis Yerusalimtsev <denis.yerusalimtsev@gmail.com>
2020-04-23 21:27:23 +0300

    Add tests for MergeTwoRddById

---
+++ spark-core/src/test/scala/com/dyerus/bigdata/sparkcore/tasks/MergeTwoRddTests.scala
@@ -0,0 +1,9 @@
+package com.dyerus.bigdata.sparkcore.tasks
+
+import com.dyerus.bigdata.sparkcore.BaseTest
+
+class MergeTwoRdd extends BaseTest {
+  behavior of MergeTwoRdd.getClass.getSimpleName
+
+  
+}
